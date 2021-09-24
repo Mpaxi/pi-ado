773c96ce489cfb46c9f299aefbce5992d032089f
Murilo Paxi <murilo.paxi@gmail.com>
2021-09-23 21:34:47 -0300

    Exercício 03

---
+++ AlgoritmosExercicios/src/Lista/Exercico03.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lista;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author muril
+ */
+public class Exercico03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+                Scanner entrada = new Scanner(System.in);
+        Integer numero1, numero2;
+
+        System.out.println("Digite o primeiro numero");
+        numero1 = entrada.nextInt();
+
+        System.out.println("Digite o segundo numero");
+        numero2 = entrada.nextInt();
+
+        System.out.println("Resultado: " + ((numero1 * numero1) + (numero2 * numero2)));
+    }
+    
+}
