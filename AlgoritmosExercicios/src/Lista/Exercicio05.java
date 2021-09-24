760ee23e9652f0eafcf7ee66a35c916d962a6fa0
Murilo Paxi <murilo.paxi@gmail.com>
2021-09-23 21:41:40 -0300

    Exercício 05

---
+++ AlgoritmosExercicios/src/Lista/Exercicio05.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lista;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author muril
+ */
+public class Exercicio05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+                Scanner entrada = new Scanner(System.in);
+        Integer numero1, numero2;
+
+        System.out.println("Digite o primeiro numero");
+        numero1 = entrada.nextInt();
+
+        System.out.println("Resultado ao quadrado: " + Math.pow(numero1, 2));
+    }
+    
+}
